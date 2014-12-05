a4f3f59f86762f63b164062041aa2baf41460c08
Roicel Laya <roicel.laya@gmail.com>
2014-12-05 02:36:57 -0430

    Implementado servicio de listar todas las cosas (en mi caso tareas).

--- models/quotes.js
+++ models/quotes.js
@@ -1,7 +1,7 @@
 //routes/reports.js
 var express = require('express');
 var cradle = require('cradle');
-var db = new (cradle.Connection)().database('quotes');
+var db = new (cradle.Connection)().database('tarea');
 
 var params = {author: process.argv[2], quote: process.argv[3]};
 
@@ -41,10 +41,36 @@ function createQuotesView(err) {
   }
 }
 */
-function outputQuotes(err) {
+
+function outputQuotes2(err) {
   console.log("Necesitamos un milagro");
 }
 
+
+function outputQuotes(res) {
+  var rows = [];
+	var self = this;
+	self.res = res;
+
+  // Creo la vista para obtener todos los valores
+  db.save('_design/tareas', {
+		all: {
+			map: function (doc) {
+				emit(doc._id, doc);
+			}
+		}
+  });
+
+  // Consulto la vista que devuelve todas las tareas
+  db.view('tareas/all', function(err, res) {
+		res.forEach(function(row) {
+			rows.push(row);
+		});
+		self.res.json({message: rows});
+  });
+}
+
+/*
 function Parametros(id, res) {
   console.log('esto es la ruta/models/post/'+id);
    res.json({ message: 'models/'+id});
@@ -60,32 +86,38 @@ function checkAndSave(err) {
 
   outputQuotes();
 }
+*/
 
-function Parametros2(id) {
+function Parametros2(id, res) {
  // errorHandler(err);
 
   if (id) {
-    db.save({author: params.author}, outputQuotes);
+    db.save({value: id}, outputQuotes2);
     return;
   }
 
-  outputQuotes();
+  console.log('Id parametros2: ', id);
 }
 
+/*
 db.exists(function (err, exists) {
   errorHandler(err);
   if (!exists) { db.create(checkAndSave); }
   checkAndSave();
 });
+*/
 
 //config Rutes
 var router = express.Router();
 router.route('/models').get(function(req, res){
-  outputQuotes();
+  outputQuotes(res);
 });
 
 router.route('/models/:id').post(function(req, res){
   var identificator = req.params.id;
+  
+  console.log('Id route: ', identificator);
+  
   Parametros2(identificator, res);
 });
 
